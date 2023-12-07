2fb86100c9b35b28262b75e5b9974a3c7594a52c
= <harmtrav@gmail.com>
2023-12-07 15:47:56 -0600

    Only export setup from userEvent

--- src/index.ts
+++ src/index.ts
@@ -2,24 +2,28 @@ import { mount as rendrjsmount, Elem } from '@rendrjs/core';
 import events from '@testing-library/user-event';
 export * as screen from '@testing-library/dom';
 
+// export const userEvent = {
+//     setup: events.setup,
+//     clear: events.clear,
+//     click: events.click,
+//     copy: events.copy,
+//     cut: events.cut,
+//     dblClick: events.dblClick,
+//     deselectOptions: events.deselectOptions,
+//     hover: events.hover,
+//     keyboard: events.keyboard,
+//     pointer: events.pointer,
+//     paste: events.paste,
+//     selectOptions: events.selectOptions,
+//     tripleClick: events.tripleClick,
+//     type: events.type,
+//     unhover: events.unhover,
+//     upload: events.upload,
+//     tab: events.tab,
+// };
+
 export const userEvent = {
-    setup: events.setup,
-    clear: events.clear,
-    click: events.click,
-    copy: events.copy,
-    cut: events.cut,
-    dblClick: events.dblClick,
-    deselectOptions: events.deselectOptions,
-    hover: events.hover,
-    keyboard: events.keyboard,
-    pointer: events.pointer,
-    paste: events.paste,
-    selectOptions: events.selectOptions,
-    tripleClick: events.tripleClick,
-    type: events.type,
-    unhover: events.unhover,
-    upload: events.upload,
-    tab: events.tab,
+    setup: () => events.setup(),
 };
 
 export const mount = (elem: Elem): HTMLElement => {
