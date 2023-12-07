1581f9e39f52de0f42c8e0d5463d065569f93315
= <harmtrav@gmail.com>
2023-12-07 17:03:36 -0600

    Export events

--- src/index.ts
+++ src/index.ts
@@ -2,24 +2,24 @@ import { mount as rendrjsmount, Elem } from '@rendrjs/core';
 import events from '@testing-library/user-event';
 export * as screen from '@testing-library/dom';
 
-export const userEvent: typeof events = {
-    setup: events.setup,
-    clear: events.clear,
-    click: events.click,
-    copy: events.copy,
-    cut: events.cut,
-    dblClick: events.dblClick,
-    deselectOptions: events.deselectOptions,
-    hover: events.hover,
-    keyboard: events.keyboard,
-    pointer: events.pointer,
-    paste: events.paste,
-    selectOptions: events.selectOptions,
-    tripleClick: events.tripleClick,
-    type: events.type,
-    unhover: events.unhover,
-    upload: events.upload,
-    tab: events.tab,
+export const userEvent: typeof events.default = {
+    setup: events.default.setup,
+    clear: events.default.clear,
+    click: events.default.click,
+    copy: events.default.copy,
+    cut: events.default.cut,
+    dblClick: events.default.dblClick,
+    deselectOptions: events.default.deselectOptions,
+    hover: events.default.hover,
+    keyboard: events.default.keyboard,
+    pointer: events.default.pointer,
+    paste: events.default.paste,
+    selectOptions: events.default.selectOptions,
+    tripleClick: events.default.tripleClick,
+    type: events.default.type,
+    unhover: events.default.unhover,
+    upload: events.default.upload,
+    tab: events.default.tab,
 };
 
 export const mount = (elem: Elem): HTMLElement => {
