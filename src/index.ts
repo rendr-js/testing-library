44fa9ac548ca164e7711d70ca7d4fc8d6531b7ee
= <harmtrav@gmail.com>
2023-12-07 16:54:39 -0600

    Move dev dependencies to dependencies

--- src/index.ts
+++ src/index.ts
@@ -2,28 +2,24 @@ import { mount as rendrjsmount, Elem } from '@rendrjs/core';
 import events from '@testing-library/user-event';
 export * as screen from '@testing-library/dom';
 
-// export const userEvent = {
-//     setup: events.setup,
-//     clear: events.clear,
-//     click: events.click,
-//     copy: events.copy,
-//     cut: events.cut,
-//     dblClick: events.dblClick,
-//     deselectOptions: events.deselectOptions,
-//     hover: events.hover,
-//     keyboard: events.keyboard,
-//     pointer: events.pointer,
-//     paste: events.paste,
-//     selectOptions: events.selectOptions,
-//     tripleClick: events.tripleClick,
-//     type: events.type,
-//     unhover: events.unhover,
-//     upload: events.upload,
-//     tab: events.tab,
-// };
-
-export const userEvent = {
-    setup: () => events.default.setup(),
+export const userEvent: typeof events = {
+    setup: events.setup,
+    clear: events.clear,
+    click: events.click,
+    copy: events.copy,
+    cut: events.cut,
+    dblClick: events.dblClick,
+    deselectOptions: events.deselectOptions,
+    hover: events.hover,
+    keyboard: events.keyboard,
+    pointer: events.pointer,
+    paste: events.paste,
+    selectOptions: events.selectOptions,
+    tripleClick: events.tripleClick,
+    type: events.type,
+    unhover: events.unhover,
+    upload: events.upload,
+    tab: events.tab,
 };
 
 export const mount = (elem: Elem): HTMLElement => {
